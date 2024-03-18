4e8f4419fbe062b9533a27f6c83bbbb5bd0776a1
“Doryan” <doryanamt@gmail.com>
2024-03-18 15:13:16 +0100

    tfg

--- tfg/league/static/league/js/index.js
+++
@@ -1,8 +0,0 @@
-// let list_champions = getElementById('list_champions');
-// let champions = "{{champions}}";
-// list_champions.innerHTML = "";
-
-// function championKey(){
-//     list_champions.style.style.backgroundColor = "red";
-
-// }
